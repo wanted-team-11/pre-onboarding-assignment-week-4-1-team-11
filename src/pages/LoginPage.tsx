6bd4590695398ff1c72ba4662ed5faf41ce8f754
gkdfo40 <gkdfo40@gmail.com>
2022-09-21 23:46:30 +0900

    feat: login page 디자인 수정 가운데 정렬과 Card Inferface를 추가함

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,67 +1,68 @@
 import { UserOutlined, LockOutlined, LoginOutlined } from "@ant-design/icons";
-import { Button, Input, Form, Row, Col } from "antd";
-import { ChangeEvent } from "react";
+import { Button, Input, Form, Card } from "antd";
+
 type formParamsType = {
   email: string;
   password: string;
 };
+
 const LoginPage = () => {
-  const onChangeEmail = (e: ChangeEvent<HTMLInputElement>) => {};
-  const onChangePassword = (e: ChangeEvent<HTMLInputElement>) => {};
   const onClickSubmit = (values: formParamsType) => {
     console.log(values);
   };
   return (
-    <div>
-      <Row justify="center" align="middle">
-        <Col span={4}>
-          <Form
-            style={{ width: 300 }}
-            name="normal_login"
-            className="login-form"
-            initialValues={{ remember: true }}
-            onFinish={onClickSubmit}
+    <div
+      style={{
+        width: "100%",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Card
+        title="Login"
+        style={{ width: 300, boxShadow: "1px 1px 10px rgba(0,0,0,0.1)" }}
+      >
+        <Form
+          name="normal_login"
+          className="login-form"
+          initialValues={{ remember: true }}
+          onFinish={onClickSubmit}
+        >
+          <Form.Item
+            name="email"
+            rules={[{ required: true, message: "Please input your Username!" }]}
           >
-            <Form.Item
-              name="email"
-              rules={[
-                { required: true, message: "Please input your Username!" },
-              ]}
-            >
-              <Input
-                type="email"
-                prefix={<UserOutlined />}
-                placeholder="아이디를 입력하세요"
-                onChange={onChangeEmail}
-              />
-            </Form.Item>
-            <Form.Item
-              name="password"
-              rules={[
-                { required: true, message: "Please input your Password!" },
-              ]}
-            >
-              <Input.Password
-                prefix={<LockOutlined />}
-                placeholder="비밀번호를 입력하세요"
-                onChange={onChangePassword}
-              />
-            </Form.Item>
+            <Input
+              type="email"
+              prefix={<UserOutlined />}
+              placeholder="아이디를 입력하세요"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "Please input your Password!" }]}
+          >
+            <Input.Password
+              prefix={<LockOutlined />}
+              placeholder="비밀번호를 입력하세요"
+            />
+          </Form.Item>
 
-            <Form.Item>
-              <Button
-                block
-                type="primary"
-                htmlType="submit"
-                className="login-form-button"
-                icon={<LoginOutlined />}
-              >
-                로그인
-              </Button>
-            </Form.Item>
-          </Form>
-        </Col>
-      </Row>
+          <Form.Item>
+            <Button
+              block
+              type="primary"
+              htmlType="submit"
+              className="login-form-button"
+              icon={<LoginOutlined />}
+            >
+              로그인
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
     </div>
   );
 };
